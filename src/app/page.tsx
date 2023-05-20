951fde7ddc50c0b39853f02be5ab1b9bfd6cc8a0
CaioGiasson <cainitech@gmail.com>
2023-05-20 10:43:26 -0300

    Commit inicial

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+
+      <div className={styles.center}>
+	Hello World
+      </div>
+
+    </main>
+  )
+}
